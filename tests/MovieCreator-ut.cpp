57a4684b54f1844c55b8cc8d90f1e5eec41dd14e
damixiorek <damixiorek@gmail.com>
2021-01-30 09:30:58 +0100

    add tests for MovieCreator class

--- tests/MovieCreator-ut.cpp
+++ tests/MovieCreator-ut.cpp
@@ -1,5 +1,21 @@
 #include <gtest/gtest.h>
+
 #include "MovieCreator.hpp"
+#include "IProduct.hpp"
+
+#include <string>
 
 class MovieCreatorTest : public ::testing::Test {
+public:
+    const std::string alienTitle = "Alien";
+    const std::string alienMovie = "Alien _movie";
 };
+
+TEST_F(MovieCreatorTest, ShouldReturnGameClassObjectWithNameFollowedByPostfix)
+{
+    MovieCreator* movieCreatorPointer = new MovieCreator();
+    IProduct* alienPointer = movieCreatorPointer->createProduct(alienTitle);
+    EXPECT_EQ(alienPointer->getTitle(), alienMovie);
+    delete alienPointer;
+    delete movieCreatorPointer;
+}
