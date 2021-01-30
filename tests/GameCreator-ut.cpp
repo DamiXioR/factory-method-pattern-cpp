8eba01c36c9f936095cb5f0b7587842c87b1e777
damixiorek <damixiorek@gmail.com>
2021-01-30 09:16:34 +0100

    add GameCreator tests

--- tests/GameCreator-ut.cpp
+++ tests/GameCreator-ut.cpp
@@ -1,5 +1,18 @@
 #include <gtest/gtest.h>
 #include "GameCreator.hpp"
+#include "IProduct.hpp"
 
 class GameCreatorTest : public ::testing::Test {
+public:
+    const std::string uoTitle = "Ultima Online";
+    const std::string uoGame = "Ultima Online _game";
 };
+
+TEST_F(GameCreatorTest, ShouldReturnGameClassObjectWithNameFollowedByPostfix)
+{
+    GameCreator* gameCreatorPointer = new GameCreator();
+    IProduct* ultimaOnlinePointer = gameCreatorPointer->createProduct(uoTitle);
+    EXPECT_EQ(ultimaOnlinePointer->getTitle(), uoGame);
+    delete ultimaOnlinePointer;
+    delete gameCreatorPointer;
+}
